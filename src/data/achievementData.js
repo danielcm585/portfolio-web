1e00991dc128bdebc1feff24817fd5b8bc92c989
danielcm585 <danielcm585@gmail.com>
2022-03-25 23:23:38 +0700

    updated achievement

--- src/data/achievementData.js
+++ src/data/achievementData.js
@@ -1,28 +1,39 @@
-const data = [{
+const data = [
+  {
     "title": "National Gold Medalist (as UNLIMITED)",
     "contest": "ICPC Asia Jakarta 2021",
     "medal": "gold",
     "site": "https://competition.binus.ac.id/icpc2021/final.html",
-  }, {
+  }, 
+  {
     "title": "Bronze Medalist",
     "contest": "Indonesia NOI 2020 (KSN 2020)",
     "medal": "bronze",
     "site": "http://www.kalderanews.com/2020/10/inilah-daftar-lengkap-pemenang-ksn-jenjang-sma-bidang-informatika-2020/",
-  }, {
+  }, 
+  {
     "title": "Top 12 Team (as UNLIMITED)",
     "contest": "INC 2021",
     "medal": "none",
     "site": "https://competition.binus.ac.id/inc2021/",
-  }, {
+  }, 
+  {
     "title": "Top 14 Indonesia Participant",
     "contest": "APIO 2021",
     "medal": "none",
     "site": "http://informatics.edu.mn/APIO_2021_Unofficial_Result.pdf",
-  }, {
+  }, 
+  {
     "title": "Top 17 Participant",
     "contest": "Indonesia IOI Qualification 2021",
     "medal": "none",
     "site": "https://edukasi.kompas.com/read/2021/05/22/215927271/17-siswa-indonesia-siap-unjuk-kemampuan-di-olimpiade-informatika-asia?page=all",
+  },
+  {
+    "title": "Best Team",
+    "contest": "Comfest Software Engineer Academy",
+    "medal": "none",
+    "site": "https://www.compfest.id/"
   }
 ]
 
